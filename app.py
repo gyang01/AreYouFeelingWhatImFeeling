a6d03dd84f953e22f0f1a89e342d7b60ed857b4d
mengdilin95@gmail.com <mengdilin95@gmail.com>
2014-02-08 00:11:14 -0500

    error in feel button :<

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template, url_for, redirect,request
+import tweetSentiment
 
 app = Flask(__name__)
 
@@ -8,13 +9,17 @@ def index():
     if request.method=="GET":
         return render_template("main.html")
     elif request.method=="POST":
+        print request.form['button']
         if request.form['button']=="emotion":
             emotion=request.form['emotionvalue']
-            return redirect(url_for("results"))
+            positive=tweetSentiment.getPositive(emotion)
+            percent=tweetSentiment.getConfidence(emotion)
+            print "here"
+            return render_template("main.html",positive=positive,confidence=percent,answer=True)
 
-@app.route("/feel")
+@app.route("/results")
 def results():
-    render_template("results.html")
+    return render_template("main.html",)
 
 if __name__=="__main__":
     app.debug=True
