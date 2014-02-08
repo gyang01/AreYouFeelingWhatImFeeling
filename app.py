f7f781037b0931cdf44dcc865165eba3263fc106
mengdilin95@gmail.com <mengdilin95@gmail.com>
2014-02-08 01:34:20 -0500

    better api

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import render_template, url_for, redirect,request
-import tweetSentiment
+import reliableApi
 
 app = Flask(__name__)
 
@@ -13,16 +13,9 @@ def index():
     if request.method=="POST":
         if request.form['emotion']=="emotion":
             emotion=request.form['emotionvalue']
-            positive=tweetSentiment.getPositive(emotion)
-            percent=tweetSentiment.getConfidence(emotion)
-            return render_template("main.html",positive=positive,confidence=percent,answer=True)
-
-@app.route("/results")
-def results(emotion):
-    positive=tweetSentiment.getPositive(emotion)
-    percent=tweetSentiment.getConfidence(emotion)
-    return render_template("main.html",positive=positive,confidence=percent,answer=True)
-
+            positive=reliableApi.getPositive(emotion)
+            score=reliableApi.getConfidence(emotion)
+            return render_template("main.html",positive=positive,confidence=score,answer=True)
 
 if __name__=="__main__":
     app.debug=True
